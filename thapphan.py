699804da6c90818ee0b3176e99d51c22e6ed29f4
SteveWang06 <91189532+SteveWang06@users.noreply.github.com>
2021-09-24 23:42:19 +0800

    Update thapphan.py

--- thapphan.py
+++ thapphan.py
@@ -5,31 +5,31 @@ print('{0:.{1}f}'.format(a,b))
 print(round(a))
 
 #cach lam tron so thap phan co xu ly dau vao
-a=input('so a la ')
-b=input('so b la ')
+a=input('A number is ')
+b=input('B number is ')
 try:
 	a=float(a)
 	b=int(b)	
 	print('{0:.{1}f}'.format(a,b))
 except:
-	print('nhap sai gia tri')
+	print('input data is wrong, try again')
 	
 #khi su dung while thi khi nhap sai so chuong trinh se cho nhap lai	
 while True:
 	try:
-		a=float(input("a la "))
+		a=float(input("A number is "))
 		break
 	except:
-		print('ban da nhap sai so a') 
-		
-		
+		print('data type of A number is wrong, try again ') 
+				
 	pass
+
 while True:
 	try:
-		b=int(input('b la '))
+		b=int(input('B number is '))
 		break
 	except:
-		print('ban da nhap sai so b')
+		print('data type of B number is wrong, try again')
 		
 	pass
 print('{0:.{1}f}'.format(a,b))
