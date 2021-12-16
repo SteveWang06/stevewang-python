1d61112b39b87e08a2e6d9923fbe691e07cbe837
SteveWang06 <91189532+SteveWang06@users.noreply.github.com>
2021-12-16 14:53:23 +0800

    Delete damlakeo.py

--- damlakeo.py
+++
@@ -1,9 +0,0 @@
-import random
-import numpy
-
-choose = ['石頭','剪刀','布']
-result = ['draw', 'lose', 'win']
-playLaw = [[0, 1, 2], [2, 0, 1], [1, 2, 0]]
-machineChoose = numpy.random.randint(3)
-playerChoose = choose.index(input('enter your choose: '))
-print('machine choose: %s \nyour choose: %s \nresult: %s \n' % (choose[machineChoose], choose[playerChoose], result[playLaw[machineChoose][playerChoose]]))
