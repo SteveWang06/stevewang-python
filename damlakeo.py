629416cd593a2e2822582dee258ead80f0c2abaa
SteveWang06 <91189532+SteveWang06@users.noreply.github.com>
2021-12-16 14:54:18 +0800

    Add files via upload

---
+++ damlakeo.py
@@ -0,0 +1,14 @@
+import random
+import numpy
+
+
+while True:
+    choose = ['石頭','剪刀','布']
+    result = ['draw', 'lose', 'win']
+    playLaw = [[0, 1, 2], [2, 0, 1], [1, 2, 0]]
+    machineChoose = numpy.random.randint(3)
+    playerChoose = choose.index(input('enter your choose: '))
+    print('machine choose: %s \nyour choose: %s \nresult: %s \n' % (choose[machineChoose], choose[playerChoose], result[playLaw[machineChoose][playerChoose]]))
+    if(result[playLaw[machineChoose][playerChoose]] == result[2]):
+        break
+        
